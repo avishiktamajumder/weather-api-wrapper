2c15dbbedc1e490bfffbda45a1c6b7a7cdc0a3d3
Avishikta <91772866+avishiktamajumder@users.noreply.github.com>
2024-08-09 01:56:25 +0530

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,16 +1,17 @@
+import os
 import requests
 import redis 
 import json
 
 #setting up Redis client
-redis_client=redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_url=os.getenv('REDIS_URL')
+redis_client=redis.StrictRedis.from_url(redis_url, ssl=True)
 
 #Function to get weather data
 def get_weather(city_code):
     cached = redis_client.get(city_code)
     if cached:
-        print("Fetching data from cache...")
-        return json.loads(cached)
+        return cached.decode('utf-8')
     
     print("Fetching data from API...")
     api_key='QT52JP3HDR5FWRK3U5WLM2HZE'
@@ -20,6 +21,6 @@ def get_weather(city_code):
     weather_data=response.json()
 
     #pushing the API response to cache
-    redis_client.setex(city_code,43200, json.dumps(weather_data)) #set 12 hour TTL for the cached data
+    redis_client.setex(city_code,43200, weather_data) #set 12 hour TTL for the cached data
 
     return weather_data
